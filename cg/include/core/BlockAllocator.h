3130314faf38c24e102d789e1aea35207643ad8b
Paulo Pagliosa <ppagliosa@gmail,com>
2023-01-19 20:58:24 -0400

    Fixing a typo in block allocator

--- cg/include/core/BlockAllocator.h
+++ cg/include/core/BlockAllocator.h
@@ -1,6 +1,6 @@
 //[]---------------------------------------------------------------[]
 //|                                                                 |
-//| Copyright (C) 2018, 2019 Paulo Pagliosa.                        |
+//| Copyright (C) 2018, 2023 Paulo Pagliosa.                        |
 //|                                                                 |
 //| This software is provided 'as-is', without any express or       |
 //| implied warranty. In no event will the authors be held liable   |
@@ -28,7 +28,7 @@
 // Class definition for block allocator.
 //
 // Author: Paulo Pagliosa
-// Last revision: 16/02/2019
+// Last revision: 19/01/2023
 
 #ifndef __BlockAllocator_h
 #define __BlockAllocator_h
@@ -177,11 +177,14 @@ public:
 
   static void free(T* ptr)
   {
-    storage_type& s = storage();
+    if (ptr != nullptr)
+    {
+      storage_type& s = storage();
 
-    s.lock();
-    s.free(ptr);
-    s.unlock();
+      s.lock();
+      s.free(ptr);
+      s.unlock();
+    }
   }
 
   static int blockCount()
